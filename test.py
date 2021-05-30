638081b3b2e51caa807ef9757efb50732c509a5a
Asaf Bialystok <abialystok@gmail.com>
2021-05-30 18:59:06 +0300

    Add serial prints

--- test.py
+++ test.py
@@ -1,31 +1,60 @@
 #!/usr/bin/python3
 
 from enum import Enum
+import serial
+import time
 
+delay = 0.2
 class Direction(Enum):
     FORWARDS = 1
     BACKWARDS = 2
     STOPPED = 3
 
+def read_serial(ser):
+    try:
+        for i in range(5):
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
+    except:
+        pass
+
 # True = forwards, False = backwards
-def manual_movement():
+def manual_movement(ser):
     direction = Direction.STOPPED
     # set speed in the beginning I think
 
     while True:
-        # TODO: switch prints with serial prints
+        time.sleep(delay)
         angle = int(input())
         if angle == 0:
-            print("stop")
+            ser.write(b"stop")
             direction = Direction.STOPPED
             continue
 
         if angle > 0 and direction != Direction.FORWARDS:
             direction = Direction.FORWARDS
-            print("forward")
+            ser.write(b"forward")
+            read_serial(ser)
         elif angle < 0 and direction != Direction.BACKWARDS:
             direction = Direction.BACKWARDS
-            print("backward")
-        print(angle)
+            ser.write(b"backward")
+            read_serial(ser)
+        angle = abs(angle)
+        time.sleep(delay)
+        ser.write(b"turn %d" % angle)
+        read_serial(ser)
+        time.sleep(delay)
+        ser.write(b"speed 200")
+        read_serial(ser)
 
-manual_movement()
+if __name__ == "__main__":
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.flush()
+    time.sleep(delay)
+    ser.write(b"speed 200")
+    read_serial(ser)
+    time.sleep(delay)
+    ser.write(b"forward")
+    read_serial(ser)
+    time.sleep(delay)
+    manual_movement(ser)
