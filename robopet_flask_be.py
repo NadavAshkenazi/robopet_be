a552684ec89315d8fdae37f75366b737033ec561
Asaf Bialystok <abialystok@gmail.com>
2021-06-08 16:47:13 +0300

    Http server that triggers processes and terminates them

--- robopet_flask_be.py
+++ robopet_flask_be.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+from dummy_actions import *
 from flask import Flask, request
+from multiprocessing import Process
+
+hostileP = Process(target=dummy_hostile)
+friendlyP = Process(target=dummy_friendly)
+processes = [hostileP, friendlyP]
 app = Flask(__name__)
+
 
 @app.route('/upload', methods=['PUT'])
 def create_user():
@@ -13,3 +20,31 @@ def create_user():
 	f = request.files['picture']
 	f.save("1")
 	return 201, "1"
+
+@app.route('/hostile', methods=['PUT'])
+def hostile():
+    for p in processes:
+        if p.is_alive():
+            p.terminate()
+
+    processes[0] = Process(target=dummy_hostile)
+    processes[0].start()
+    return "OK", 204
+
+@app.route('/friendly', methods=['PUT'])
+def friendly():
+    for p in processes:
+        if p.is_alive():
+            p.terminate()
+
+    processes[1] = Process(target=dummy_friendly)
+    processes[1].start()
+    return "OK", 204
+
+@app.route('/sleep', methods=['PUT'])
+def sleep():
+    for p in processes:
+        if p.is_alive():
+            p.terminate()
+
+    return "OK", 204
---
+++ run_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+FLASK_APP=robopet_flask_be.py
+flask run -h 0.0.0.0 -p 3000
