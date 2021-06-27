d5c8f3fb5e308e95e3cbbb708c66406fbae11ed4
Asaf Bialystok <abialystok@gmail.com>
2021-06-27 22:40:29 +0300

    hostile

--- behaviors.py
+++ behaviors.py
@@ -8,6 +8,7 @@ from RobopetFaceDetect.main import getLocation, getLocationHostile
 from arduinoInfra import turn_30_right, turn_30_left
 import threading
 from multiprocessing import Process
+from pygame import mixer
 
 MIN_X_ANGLE = 0
 MAX_X_ANGLE = 180
@@ -16,8 +17,8 @@ MAX_Y_ANGLE = 90
 CAMERA_STEP = 20
 
 
-def _bark():
-    t = threading.Thread(target=make_repetitive_sounds, args=(Sound.BARK_TWICE, 2.5))
+def _bark(sound=Sound.BARK_TWICE, time=2.5):
+    t = threading.Thread(target=make_repetitive_sounds, args=(sound, time))
     t.start()
     time.sleep(0.5)
     for i in range(4):
@@ -61,13 +62,10 @@ def search_face_hostile():
     time.sleep(2)
     ser.write("cam_setY 75")
     ser.write("cam_setX 90")
-    location = getLocationHostile(3)
-    if location is not None:
-        print("Found stranger")
-        print(location)
-        return location, 90
-
-    return None
+    location, id, confidence = getLocationHostile(3)
+    print("Found stranger")
+    print(location)
+    return id, confidence
 
 
 def search_face():
@@ -134,30 +132,37 @@ def align_by_location(location):
 def behave_hostile():
     ser = mySerial()
     ser.init_serial()
-    time.sleep(1)
-    ser.write("eyes red")
-    ser.write("eyes red")
-    time.sleep(1)
-    p = Process(target=make_repetitive_sounds, args=(Sound.GROWL, 30))
+    ser.write("mouthSet 60")
+    t = threading.Thread(target=make_repetitive_sounds, args=(Sound.GROWL, 30))
+    t.start()
+    #p = Process(target=make_repetitive_sounds, args=(Sound.GROWL, 30))
     location = search_face()
     if location is None:
         _bark()
         return
 
     align_by_location(location)
-    res = search_face_hostile()
-    if p.is_alive():
-        p.terminate()
-    if res is not None:
-        t = threading.Thread(target=make_repetitive_sounds, args=(Sound.SCARY_BARK, 5))
+    id, confidence = search_face_hostile()
+    print(f"id is {id}")
+    print(f"confidence is {confidence}")
+#    if p.is_alive():
+#        p.terminate()
+    mixer.init()
+    mixer.music.stop()
+    if id <= 0 or confidence > 100:
+        _bark(Sound.SCARY_BARK, 5)
+        ser.write("eyes red")
+        # t = threading.Thread(target=make_repetitive_sounds, args=(Sound.SCARY_BARK, 5))
         t.start()
         for i in range(3):
             ser.write("forward")
             time.sleep(0.5)
             ser.write("backward")
+            time.sleep(0.5)
         ser.write("stop")
     else:
-        t = threading.Thread(target=make_repetitive_sounds, args=(Sound.HAPPY_BARK, 5))
+        _bark(Sound.HAPPY_BARK, 5)
+        # t = threading.Thread(target=make_repetitive_sounds, args=(Sound.HAPPY_BARK, 5))
         t.start()
         ser.write("eyes green")
         ser.write("shakeTail")
